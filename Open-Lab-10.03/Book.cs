3b1be34bcfa4cb5ca4e18875dfc7ea18f60a1514
Andrej Svrček <nepoviem251@gmail.com>
2021-12-01 23:24:21 +0100

    Add project files.

---
+++ Open-Lab-10.03/Book.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._03
+{
+    class Book
+    {
+        private string title;
+        private int pages;
+        private string category;
+        private string author;
+        private int releaseDate;
+        
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+        public int Pages
+        {
+            get
+            {
+                return pages;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    pages = 1;
+                }
+                else
+                {
+                    pages = value;
+                }
+            }
+        }
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+            set
+            {
+                category = value;
+            }
+        }
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+        public int ReleaseDate
+        {
+            get
+            {
+                return releaseDate;
+            }
+            set
+            {
+                if (value > 2021 || value < 1450)
+                {
+                    releaseDate = -1;
+                }
+                else
+                {
+                    releaseDate = value;
+                }
+            }
+        }
+    }
+ }
+
---
+++ Open-Lab-10.03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Open_Lab_10._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book();
+            LOTR.Title = "Kďe všade som (ne)zomrel";
+            LOTR.Pages = -269;
+            LOTR.Category = "Tragikomédia";
+            LOTR.Author = "PPPeter";
+            LOTR.ReleaseDate = 2077;
+
+            Console.WriteLine(LOTR.Title);
+            Console.WriteLine(LOTR.Pages);
+            Console.WriteLine(LOTR.Category);
+            Console.WriteLine(LOTR.Author);
+            Console.WriteLine(LOTR.ReleaseDate);
+        }
+    }
+}
